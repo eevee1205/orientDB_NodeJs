c93a2000f8f9d90ba95e4ce7d797f3a649cbcfe9
eevee1205 <eeve1205@naver.com>
2018-07-15 00:00:53 +0900

    20180715

--- app_orientdb.js
+++ app_orientdb.js
@@ -25,72 +25,49 @@ app.listen(3000, function(){  //3000포트에 연결
 });
 
 
-app.get('/topic/new', function(req, res){
-  fs.readdir('data', function(err, files){
-    if(err){
-      res.status(500).send('Server Error!!!');
+app.get('/topic/add', function(req, res){
+  var sql = "SELECT FROM topic";
+  db.query(sql).then(function(topics){
+    if(topics.length == 0){
+      console.log('No Data');
+      res.status(500).send('데이터가 없습니다.');
     }
-    res.render('new', {topicList:files});
+    res.render('add', {topics:topics});
   });
 });
 
+
+app.post('/topic/add' , function(req,res){
+  var title = req.body.title;
+  var description = req.body.description;
+  var author = req.body.author;
+
+  var param = {
+    params:{
+      title:title,
+      description: description,
+      author : author
+    }
+  }
+  var sql = "INSERT INTO topic (title, description, author) VALUES(:title, :description, :athor)";
+  db.query(sql , param).then(function(results){
+    //res.send(results);
+    res.redirect('/topic/'+encodeURIComponent(results[0]))
+  });
+});
 app.get(['/topic', '/topic/:id'], function(req, res){ //2가지 routing 을 하나의 함수로
   var sql = "SELECT FROM topic";
   db.query(sql).then(function(topics){
-    res.render('view' , {topicList:topics}) //결과를 view.jade 로 전달
-  });
-  /*
-  fs.readdir('data', function(err, files){
-    if(err){
-      res.status(500).send('Server Error!!!');
-    }
+    //res.render('view' , {topicList:topics}) //결과를 view.jade 로 전달
 
-    var id = req.params.id; //id값을 가져와 분기처리
-    if(id){
-    //id 값이 있을때
-    fs.readFile('data/'+id, 'utf-8', function(err , data){//파일읽기 성공 후 클릭한 파일을 읽는다.
-      if(err){
-        res.status(500).send('FileRead Error!!!');
-      }
-      res.render('view', {topicList:files, title:id, description:data}); //파일을 읽은 후 view경로에 있는 jade 파일을 읽는다. 객체로 데이터 전송
-    });
-  }else {
-      //id 값이 없을때
-      res.render('view',{topicList:files, title:'Welcome', description:'Hello, JavaScript Server'});
-    }
-  })
-  */
-});
-
-/*
-  app.get('/topic/:id' ,function(req, res){
     var id = req.params.id;
-
-    fs.readdir('data', function(err, files){  ///topic/:id 경로로 접근시 data 경로에 있는 파일을 읽는다.
-      if(err){
-        res.status(500).send('Server Error!!!');
-      }
-
-      fs.readFile('data/'+id, 'utf-8', function(err , data){//파일읽기 성공 후 클릭한 파일을 읽는다.
-        if(err){
-          res.status(500).send('FileRead Error!!!');
-        }
-        res.render('view', {topicList:files, title:id, description:data}); //파일을 읽은 후 view경로에 있는 jade 파일을 읽는다. 객체로 데이터 전송
+    if(id){
+      var sql = "SELECT FROM topic WHERE @rid=:rid";
+      db.query(sql, {params:{rid:id}}).then(function(topic){
+          res.render('view' , {topics:topics , topic:topic[0]});
       });
-    });
-
-
-  });
-*/
-app.post('/topic' , function(req,res){
-  var title = req.body.title;
-  var description = req.body.description;
-  fs.writeFile('data/'+title, description, function(err){
-    if(err){
-      console.log(err);
-      res.status(500).send('Server Error');
     }else{
-      res.redirect('/topic');
+        res.render('view' , {topics:topics});
     }
 
   });
